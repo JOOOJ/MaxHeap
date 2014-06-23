aed6b54b9c33ab3e86d4bff75ecf35c378b92504
JOOOJ <jjjrachel@hotmail.com>
2014-06-23 18:29:50 +0800

    Add Remove test on main function

--- HeapDemo/Program.cs
+++ HeapDemo/Program.cs
@@ -10,7 +10,8 @@ namespace HeapDemo
     {
         static void Main(string[] args)
         {
-            int[] array = { 10, 35, 7, 11, 15, 21, 3,9,23,71,2,0,6 };
+            int[] array = { 10, 35, 7, 11, 15, 21, 3, 9, 23, 71, 2, 0, 6 };
+            //int[] array = { 6, 5, 3, 1, 8, 7, 2, 4 };
             MaxHeap<int> heap = new MaxHeap<int>(array);
             foreach (int item in heap)
             {
@@ -18,6 +19,12 @@ namespace HeapDemo
             }
             Console.WriteLine("=========================");
             heap.Sort();
+            foreach (int item in heap)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("=====================");
+            heap.Remove(21);
             foreach (int item in heap)
             {
                 Console.WriteLine(item);
